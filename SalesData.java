7b8ca30be04f91adaa6ad4e6d7b8554fa9e802be
amcohen4 <allen.m.cohen@gmail.com>
2017-05-25 17:02:32 -0700

    Add files via upload

---
+++ SalesData.java
@@ -0,0 +1,20 @@
+
+/**
+ * Represent the sales data for a given period.
+ */
+public class SalesData {
+	int data[] = {0, 4, 42, 84};
+	
+	public void display() {
+		System.out.println("Data: ");
+
+		int sum = 0;
+		for (int i: data) {
+			System.out.println("Next value: " + i);
+			sum += i;
+		}
+		
+		System.out.println("Sum is: " + sum);
+	}
+
+}
